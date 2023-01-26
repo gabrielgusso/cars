a20177380e38aa286cc60fb6a48b194dd0c74324
gabrielgusso <gabriel.gusso@hotmail.com>
2023-01-26 13:44:56 -0300

    finished exercise

---
+++ src/repository/carRepository.ts
@@ -0,0 +1,50 @@
+import prisma from "../config/database.js";
+
+async function getCars() {
+  return prisma.cars.findMany()
+}
+
+async function getCar(id: number) {
+  return prisma.cars.findFirst({
+    where: {
+      id
+    }
+  })
+}
+
+async function getCarWithLicensePlate(licensePlate: string) {
+  return prisma.cars.findFirst({
+    where: {
+      licensePlate
+    }
+  })
+}
+
+async function createCar(model: string, licensePlate: string, year: number, color: string) {
+  return prisma.cars.create({
+    data: {
+      model,
+      licensePlate,
+      year: year.toString(),
+      color
+    }
+  })
+}
+
+async function deleteCar(id: number) {
+  return prisma.cars.delete({
+    where: {
+      id
+    }
+  })
+}
+
+const carRepository = {
+  getCar,
+  getCarWithLicensePlate,
+  getCars,
+  createCar,
+  deleteCar
+}
+
+export default carRepository;
